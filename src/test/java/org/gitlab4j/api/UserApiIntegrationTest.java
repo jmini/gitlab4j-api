19c9013a748b5b3fba12bb53a14161970a9dd8c0
Jeremie Bresson <jeremie.bresson@unblu.com>
2023-11-20 07:11:44 +0100

    Add tests to UserApiIntegrationTest

--- src/test/java/org/gitlab4j/api/UserApiIntegrationTest.java
+++ src/test/java/org/gitlab4j/api/UserApiIntegrationTest.java
@@ -1,5 +1,10 @@
 package org.gitlab4j.api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.gitlab4j.api.models.User;
+import org.gitlab4j.api.models.Version;
 import org.gitlab4j.test.GitLabContainer;
 import org.gitlab4j.test.GitLabInvocationContextProvider;
 import org.junit.jupiter.api.TestTemplate;
@@ -8,12 +13,32 @@ import org.junit.jupiter.api.extension.ExtendWith;
 @ExtendWith(GitLabInvocationContextProvider.class)
 class UserApiIntegrationTest {
 
-	
-	@TestTemplate
-	void testName(GitLabContainer container) throws Exception {
-	
-		System.out.println(container.version);
-		
-	}
+    private static final String TEST_USERNAME = "root";
+
+    @TestTemplate
+    public void testGetVersion(GitLabContainer container) throws GitLabApiException {
+        GitLabApi gitLabApi = new GitLabApi(container.url(), container.rootUserToken);
+        Version version = gitLabApi.getVersion();
+        assertNotNull(version);
+        System.out.format("version=%s, revision=%s%n", version.getVersion(), version.getRevision());
+        assertEquals(container.version, version.getVersion());
+        assertNotNull(version.getRevision());
+    }
+
+    @TestTemplate
+    public void testGetCurrentUser(GitLabContainer container) throws GitLabApiException {
+        GitLabApi gitLabApi = new GitLabApi(container.url(), container.rootUserToken);
+        User currentUser = gitLabApi.getUserApi().getCurrentUser();
+        assertNotNull(currentUser);
+        assertEquals(TEST_USERNAME, currentUser.getUsername());
+    }
+
+    @TestTemplate
+    public void testLookupUser(GitLabContainer container) throws GitLabApiException {
+        GitLabApi gitLabApi = new GitLabApi(container.url(), container.rootUserToken);
+        User user = gitLabApi.getUserApi().getUser(TEST_USERNAME);
+        assertNotNull(user);
+        assertEquals(TEST_USERNAME, user.getUsername());
+    }
 
 }
